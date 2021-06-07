f7b07cfec6782eaf3df4d8ef60aefacb795935f5
gerbcov <gerebcov.e.a@gmail.com>
2021-06-07 20:14:28 +0300

    Modified EnamyFighter.cs

--- Assets/scripts/EnamyFighter.cs
+++ Assets/scripts/EnamyFighter.cs
@@ -7,6 +7,8 @@ public class EnamyFighter : StateMachine
 
     [SerializeField]
     GameTrigger playerTrigger;
+    [SerializeField]
+    GameTrigger agresiveTrigger;
 
     [SerializeField]
     float moveRangeMin, moveRangeMax;
@@ -17,13 +19,13 @@ public class EnamyFighter : StateMachine
     [SerializeField]
     Weapon weapon;
 
-    float targetPosition;
     Unit player;
-    int vector = 1;
+    float vector = 1;
 
     enum States
     {
         Idle,
+        GoToPlayer,
         Attack,
         Find
     }
@@ -31,7 +33,8 @@ public class EnamyFighter : StateMachine
     private void Start()
     {
         InitState((int)States.Idle, new State(StartIdle, UpdateIdle, EndIdle));
-        InitState((int)States.Attack, new State(null, null, null));
+        InitState((int)States.GoToPlayer, new State(StartGoToPlayer, UpdateGoToPlayer, null));
+        InitState((int)States.Attack, new State(StartAttack, UpdateAttack, EndAttack));
         InitState((int)States.Find, new State(null, null, null));
 
         SetState((int)States.Idle);
@@ -40,7 +43,6 @@ public class EnamyFighter : StateMachine
     void StartIdle()
     {
         playerTrigger.OnEnterObject += PlayerTrigger_OnEnterObject;
-        targetPosition = moveRangeMin;
     }
 
     void UpdateIdle()
@@ -59,15 +61,66 @@ public class EnamyFighter : StateMachine
         playerTrigger.OnEnterObject -= PlayerTrigger_OnEnterObject;
     }
 
-
-
     private void PlayerTrigger_OnEnterObject(BaseObject obj)
     {
         var unit = obj as Unit;
         if(unit != null)
         {
             player = unit;
+            SetState((int)States.GoToPlayer);
+        }
+    }
+
+    private void StartGoToPlayer()
+    {
+        agresiveTrigger.OnExitObject += AgresiveTrigger_OnExitObject; ;
+    }
+
+    private void AgresiveTrigger_OnExitObject(BaseObject obj)
+    {
+        if(obj == player)
+        {
+            player = null;
+            SetState((int)States.Idle);
+            agresiveTrigger.OnExitObject -= AgresiveTrigger_OnExitObject; ;
+        }
+    }
+
+    private void UpdateGoToPlayer()
+    {
+        var playerVector = player.transform.position.x - unit.transform.position.x;
+
+        if (Mathf.Abs(playerVector) > atackDistance || Mathf.Sign(playerVector) != vector)
+        {
+            vector = Mathf.Sign(playerVector);
+            if ((vector > 0 && unit.transform.position.x < moveRangeMax) ||
+                (vector < 0 && unit.transform.position.x > moveRangeMin))
+                unit.Move(vector);
+        }
+        else
+        {
             SetState((int)States.Attack);
         }
     }
+
+    private void StartAttack()
+    {
+        weapon.StartFire();
+    }
+
+    private void UpdateAttack()
+    {
+        var playerVector = player.transform.position.x - unit.transform.position.x;
+
+        if (Mathf.Abs(playerVector) > atackDistance)
+        {
+            SetState((int)States.GoToPlayer);
+        }
+    }
+
+    private void EndAttack()
+    {
+        weapon.StopFire();
+    }
+
 }
